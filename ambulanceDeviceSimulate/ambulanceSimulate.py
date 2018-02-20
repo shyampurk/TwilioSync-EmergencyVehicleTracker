fb8f9165999bd29b5b3aafa8f56290313f981d2f
suryasundarraj <suryasundarraj@live.com>
2018-02-20 18:47:26 +0530

    new files

--- ambulanceDeviceSimulate/ambulanceSimulate.py
+++ ambulanceDeviceSimulate/ambulanceSimulate.py
@@ -8,15 +8,18 @@ import argparse
 from mclora import MCLoRa
 import serial
 
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "AC18af524b5da6e4429e4c5875e2fdd7cd"
+auth_token = "4ee302404ec79b0622eae81ff717e534"
+deviceID = "ISd136efcec806480a96570a44a4b942af"
+
 def internet_on():
     try:
         urllib2.urlopen('http://google.com', timeout=1)
         return True
     except urllib2.URLError as err: 
         return False
-# Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC18af524b5da6e4429e4c5875e2fdd7cd"
-auth_token = "4ee302404ec79b0622eae81ff717e534"
+
 client = Client(account_sid, auth_token)
 
 new_data = {
@@ -26,7 +29,8 @@ new_data = {
 }
 
 # 200 mts, 100 mts, 50 mts, 0 mts, -300 mts
-ambulance_list = [(37.754660, -122.399898),(37.754579, -122.401350),(37.754505, -122.402470),(37.754503, -122.402528),(37.754512, -122.402560),(37.754277, -122.406388)]
+#ambulance_list = [(37.754660, -122.399898),(37.754505, -122.402470),(37.754503, -122.402528),(37.754277, -122.406388),(37.754277, -122.406388),(37.754277, -122.406388),(37.754277, -122.406388)]
+ambulance_list = [(37.754512, -122.402560),(37.753925, -122.399577),(37.754228, -122.399612),(37.754660, -122.399898),(37.754579, -122.401350),(37.754541, -122.402021),(37.754512, -122.402560),(37.754485, -122.402862),(37.754430, -122.403747),(37.754277, -122.406388),(37.754586, -122.406395)]
 # create parser
 descStr = "Add description here"
 parser = argparse.ArgumentParser(description=descStr)
@@ -58,12 +62,10 @@ while True:
 		for i in range(len(ambulance_list)):
 			new_data["lat"] = ambulance_list[i][0]
 			new_data["lon"] = ambulance_list[i][1]
-			if(i == len(ambulance_list)):
-				time.sleep(10)
 			if internet_on():		
 				try:
 					document = client.sync \
-					    .services("ISd136efcec806480a96570a44a4b942af") \
+					    .services(deviceID) \
 					    .documents("gpsData") \
 					    .update(data=new_data)
 					print(document.data)
